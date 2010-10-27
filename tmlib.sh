d2564240fb1c719583969f8d7beaac7825abc0aa
Scott Bronson <brons_git@rinspin.com>
2010-10-27 00:19:51 -0700

    remove ASSERT from tmlib.sh

--- tmlib.sh
+++ tmlib.sh
@@ -11,36 +11,11 @@
 
 # tmlib functions:
 #
-# ASSERT:   Stop a test if a condition fails
 # TRAP:     Execute a command when an exception happens
 # ATEXIT:   Ensure a command runs even if the test fails (usually to clean up).
 # MKFILE:   Creates a temporary file with the given contents.
 # MKDIR:    create a temporary directory
 # REPLACE:  replaces literal text with other literal text (no regexes).
-
-
-
-#
-# ASSERT
-#
-# Ensure a condition is met.  If not, stop testing immediately.
-#
-# usage:
-#   . assert.sh
-# 	assert 42 -eq $((0x2A))		# true, so test continues as normal
-# 	assert 1 -eq 2				# false, so the test is aborted.
-
-ASSERT ()
-{
-	if [ ! "$@" ]; then
-		msg=''
-		if [ ${BASH_VERSINFO[0]} -ge 3 ]; then
-			# bash2 doesn't provide BASH_SOURCE or BASH_LINENO
-			msg=" on ${BASH_SOURCE[1]} line ${BASH_LINENO[0]}"
-		fi
-		ABORT assertion failed$msg: \"$@\"
-	fi
-}
 
 
 #
